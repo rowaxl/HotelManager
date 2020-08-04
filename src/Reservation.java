e86a9c801d8cd623d2b9f679c36f31fc5978b161
akaaariii <caaari23@gmail.com>
2020-08-04 15:17:10 -0700

    Implement data classes

---
+++ src/Reservation.java
@@ -0,0 +1,55 @@
+import java.util.Date;
+
+public class Reservation {
+	
+	private int reservationId;
+	private int roomNo;
+	private String emailAddress;
+	private Date checkInDate;
+	private Date checkOutDate;
+	private double price;
+	private int person;
+	
+	public Reservation(int reservationId, int roomNo, String emailAddress, Date checkInDate, Date checkOutDate,
+			double price, int person) {
+		super();
+		this.reservationId = reservationId;
+		this.roomNo = roomNo;
+		this.emailAddress = emailAddress;
+		this.checkInDate = checkInDate;
+		this.checkOutDate = checkOutDate;
+		this.price = price;
+		this.person = person;
+	}
+
+	public int getReservationId() {
+		return reservationId;
+	}
+
+	public int getRoomNo() {
+		return roomNo;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public Date getCheckInDate() {
+		return checkInDate;
+	}
+
+	public Date getCheckOutDate() {
+		return checkOutDate;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public int getPerson() {
+		return person;
+	}
+	
+	
+
+}
